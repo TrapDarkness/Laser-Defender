85b3d02ec45c26f4263168a156ed090e026dcc08
Alexander Shumway <alshumway@darwinsmonkey.net>
2018-09-23 18:37:15 -0400

    Added the ability fro multiple waves and paths for the enemies

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -5,16 +5,52 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour {
 
     // config params
+    [SerializeField] WaveConfig waveConfig;
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
+
 
 
     // Use this for initialization
     void Start () {
-		
+
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+
+        Move();
+
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards
+                (transform.position, targetPosition, movementThisFrame);
+            if (transform.position == targetPosition)
+            {
+
+                waypointIndex++;
+
+            }
+
+        }
+        else
+        {
+
+            Destroy(gameObject);
+
+        }
+   
+    }
 }
